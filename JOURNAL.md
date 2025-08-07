212192c5e69d3a5183f504ae38fadb7ac545fc20
yal212 <yal212yal@gmail.com>
2025-08-07 16:18:40 +0800

    Add new solutions for LeetCode problems: merge, maxProfit, and containsDuplicate; create a journal to track coding progress and TODOs.

---
+++ JOURNAL.md
@@ -0,0 +1,20 @@
+Journal to keep track of my coding progress :)
+
+---
+
+## TODO
+### Arrays
+- 238
+- 53
+- 15
+
+---
+
+## Aug 7
+### Leetcode
+#### Arrays
+- 121
+- 88
+- 217
+- 15 (TLE)
+---
---
+++ leetcode/121.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        buy = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            else:
+                profit = max(profit, prices[i]-buy)
+        return profit
